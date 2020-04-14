1af201dc5bc5251fbfc85a43d1139718e0f7762b
mrmedjbour <samanthabladus@gmail.com>
2020-04-14 17:58:26 +0100

    Start BreadCrumb

---
+++ resources/views/comp/breadcrumb.blade.php
@@ -0,0 +1,14 @@
+<div id="breadCrumb" style="margin-top: 2em;padding-bottom: 20px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-auto">
+                <div>
+                    <ul class="list-inline text-capitalize d-flex align-items-center" id="breadCrumbList" style="margin-bottom: 0;font-size: 0px;">
+                        <li class="list-inline-item" id="beadCrumbHome"><a href="{{route('index')}}"><i class="fa fa-home fa-lg" style="color: #58ba25;"></i></a></li>
+                        <li class="list-inline-item" id="beadCrumbPage"><a href="#">» Search Results<br></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
