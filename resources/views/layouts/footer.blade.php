7a150013b1f8b70716b4371ac1b9128d07eae582
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 16:19:00 +0100

    3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -57,7 +57,7 @@
         </div>
     </div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="{{ asset('js/bs-init.js') }}"></script>
 @if( Route::currentRouteName() == 'index' )
