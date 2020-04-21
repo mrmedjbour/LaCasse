a865ed1cbea8ab08f4a2d4b569ef3e8ddc97512d
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 16:06:48 +0100

    3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,17 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i">
-    <link rel="stylesheet" href="{{ secure_asset('fonts/fontawesome5-overrides.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/fontawesome5-overrides.min.css') }}">
     <!-- Styles -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
-    <link rel="stylesheet" href="{{ secure_asset('css/breadcrumb.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/Navigation-e-commerce.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/pagination.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/styles.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/styleSendMsgBox.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/x-dropdown.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/breadcrumb.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Navigation-e-commerce.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pagination.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styleSendMsgBox.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/x-dropdown.css') }}">
     @if(Route::currentRouteName() == 'directory' OR Route::currentRouteName() == 'profile')
         @mapstyles
     @endif
