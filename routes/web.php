7f4b6682fd1c837b8bf71b7fe383fe08d288c5a7
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 20:14:14 +0100

    api routes.

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 /*  Auth Route */
 Auth::routes();
 
