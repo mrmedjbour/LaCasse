527fa3db71c2ac611dd875b6be48e6ea9bde0c24
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 19:02:43 +0100

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (true) {
+        if (env('APP_ENV') == 'production') {
             URL::forceScheme('https');
         }
 
