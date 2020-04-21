f9db48623e17084a2ef1a982d5ceabc9608fbccd
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 22:11:24 +0100

    production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,9 +32,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        if (App::environment('production')) {
-//            URL::forceScheme('https');
-//        }
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
 
         View::composer('*', function ($view) {
             if (Auth::check()) {
