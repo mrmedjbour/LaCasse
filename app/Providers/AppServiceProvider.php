890e3d7c4a678142cfb5ad1664e63691de250e63
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 18:19:29 +0100

    Some Fix...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        if (Request::secure()) {
+        if (true) {
             URL::forceScheme('https');
         }
 
