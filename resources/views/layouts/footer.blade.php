13cf14dac0f53efa36ff1dad0192f233205fcdfe
mrmedjbour <samanthabladus@gmail.com>
2020-04-21 16:24:21 +0100

    3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -60,9 +60,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="{{ asset('js/bs-init.js') }}"></script>
-@if( Route::currentRouteName() == 'index' )
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
-@endif
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
 <script src="{{ asset('js/Card-Carousel.js') }}"></script>
 <script src="{{ asset('js/custom.js') }}"></script>
 @if( Route::currentRouteName() == 'messages' )
